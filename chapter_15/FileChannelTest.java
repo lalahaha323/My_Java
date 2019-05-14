065d2d138695bd5217e8b6b6183e71069193916c
lalahaha323 <2665687186@qq.com>
2019-05-14 21:56:11 +0800

    NIO

---
+++ chapter_15/FileChannelTest.java
@@ -0,0 +1,31 @@
+package chapter_15;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+
+public class FileChannelTest {
+    public static void main(String[] args) {
+        File f = new File("src/chapter_15/FileChannelTest.java");
+        try(
+                FileChannel inChannel = new FileInputStream(f).getChannel();
+                FileChannel outChannel = new FileOutputStream("a.txt").getChannel()
+                ) {
+            MappedByteBuffer buffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, f.length());
+            Charset charset = Charset.forName("GBK");
+            outChannel.write(buffer);
+            buffer.clear();
+            CharsetDecoder decoder = charset.newDecoder();
+            CharBuffer charBuffer = decoder.decode(buffer);
+            System.out.println(charBuffer);
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
