be6b487c69f6f5478d617c24f52309509856adbc
lalahaha323 <2665687186@qq.com>
2019-05-14 09:05:42 +0800

    序列化以及文件的追加

---
+++ chapter_15/AppendContent.java
@@ -0,0 +1,15 @@
+package chapter_15;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class AppendContent {
+    public static void main(String[] args) {
+        try(RandomAccessFile raf = new RandomAccessFile("out.txt", "rw")) {
+            raf.seek(raf.length());
+            raf.write("追加的内容！\n".getBytes());
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
